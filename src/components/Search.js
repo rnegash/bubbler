d7e378627cc94be472533d8f68bec377ff47dac5
rnegash <rnegash@macbookproc02w80gehv2g.berlin.internal.babbel.com>
2018-05-29 17:39:38 +0200

    image call working

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,39 +1,102 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
+import api from "gettyimages-api";
+
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      associations: [],
-      value: "" 
-    }
+      relatedWords: [],
+      relatedImages: [],
+      value: ""
+    };
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
+  makeWordnikCall(url) {
+    axios.get(url).then(res => {
+      let relatedWords = res.data[res.data.length - 1].words;
+      this.setState({ relatedWords });
+    });
+  }
+
+  makeWikiCall(url) {
+    axios.get(url).then(res => {
+      let result = res.data;
+      console.log(result);
+    });
+  }
+
+  makeGettyCall(word) {
+    // var api = require("gettyimages-api");
+    var creds = {
+      apiKey: "fzn7csa34kff7eh2ftz86pxs",
+      apiSecret: "5rWNb6YTP8QMSE4fYJrxXJnj8J4uqZe46TSZ5DW2nWY9K",
+      username: "rnegash",
+      password: "babbelberlin2018"
+    };
+    var client = new api(creds);
+    client
+      .searchimages()
+      .withPage(1)
+      .withPageSize(1)
+      .withPhrase("beach")
+      .execute()
+      .then(
+        response => {
+          console.log(JSON.stringify(response.images[0]));
+        },
+        err => {
+          throw err;
+        }
+      );
+
+    // axios.get(url).then(res => {
+    //   let relatedImages = res.data;
+    //   console.log(relatedImages);
+    //
+    //   //this.setState({ relatedWords });
+    // });
+  }
+
   handleClick(e) {
-    var apiUrl = "http://api.wordnik.com/v4/word.json/" + this.state.value + "/relatedWords?api_key=9ad74996eed8057e662010fa8ef0770fd099c0190d9f3f71f"
-    console.log(apiUrl)
     e.preventDefault();
-    console.log(this.state.value)
-    axios.get(apiUrl).then(res => {
-      let associations = res.data[res.data.length-1].words; 
-      this.setState({ associations });
-      })
+    let searchWord = this.state.value;
+    let wikiUrl =
+      "https://en.wikipedia.org/w/api.php?action=query&prop=images&titles=" +
+      searchWord +
+      "&format=json";
+    let wordnikUrl =
+      "http://api.wordnik.com/v4/word.json/" +
+      searchWord +
+      "/relatedWords?api_key=9ad74996eed8057e662010fa8ef0770fd099c0190d9f3f71f";
+    let gettyUrl =
+      "https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=" +
+      searchWord;
+    this.makeWordnikCall(wordnikUrl);
+    this.makeWikiCall(wikiUrl);
   }
 
   handleChange(e) {
-    this.setState({value: e.target.value});
+    this.setState({ value: e.target.value });
   }
 
   render() {
     return (
       <div>
-        <input className="input" type="text" placeholder="Text input" value={this.state.value} onChange={this.handleChange} />
-        <a onClick={this.handleClick} className="button">Button</a>
+        <input
+          className="input"
+          type="text"
+          placeholder="Text input"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <a onClick={this.handleClick} className="button">
+          Button
+        </a>
         <ul>
-          {this.state.associations.map(association => <li>{association}</li>)}
-
+          {this.state.relatedWords.map(relatedWord => <li>{relatedWord}</li>)}
         </ul>
       </div>
     );
@@ -42,5 +105,5 @@ class Search extends Component {
 
 export default Search;
 
-  //
-  // {this.state.associations.map(association => <li>{association[i]}</li>)}
+//
+// {this.state.associations.map(association => <li>{association[i]}</li>)}
