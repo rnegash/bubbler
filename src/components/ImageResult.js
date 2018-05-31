5010a39bf98dbfd516c04cc4453717ca0cab9f4a
Karen Hoyos <khoyos@babbel.com>
2018-05-31 16:04:14 +0200

    Added ImageResult Component

---
+++ src/components/ImageResult.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class ImageResult extends Component {
+  render() {
+    return (
+      <div className="container">
+       	<ul>
+        	{this.props.searchResult.map(relatedImage => (
+             	<li>
+                 	<img src={relatedImage} />
+                 </li>
+            ))}
+       	</ul>
+      </div>
+    );
+  }
+}
+
+export default ImageResult;
