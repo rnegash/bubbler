a47257d62c66cf0a163153d6f372c1a1e4ae32dc
Karen Hoyos <khoyos@babbel.com>
2018-05-31 16:05:31 +0200

    Adjusted App to display different components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
 import React, { Component } from "react";
 import Search from "./components/Search.js";
 import Header from "./components/Header.js";
+import ImageResult from "./components/ImageResult.js";
+import TextResult from "./components/TextResult.js";
+import axios from "axios";
 import "./App.css";
 import "bulma/css/bulma.css";
 
 class App extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      relatedWords: [],
+      relatedImages: [],
+      value: ""
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  makeWordnikCall(url) {
+    axios.get(url).then(res => {
+      let relatedWords = res.data[res.data.length - 1].words;
+      this.setState({ relatedWords });
+    });
+  }
+
+  makeWikiCall(url) {
+    axios.get(url).then(res => {
+      let result = res.data.query.pages;
+      console.log(result);
+      let pageId = [];
+      let relatedImages = [];
+
+      // getting the ID of the image object
+      for (var keys in result) {
+        pageId.push(keys);
+      }
+      console.log(pageId);
+
+      // get the list of image names
+      for (var i = 0; i < pageId.length; i++) {
+        console.log(result[pageId[i]].imageinfo[0].url);
+        var imageUrl = result[pageId[i]].imageinfo[0].url;
+        relatedImages.push(imageUrl);
+      }
+
+      this.setState({ relatedImages });
+
+      console.log("rel img ", relatedImages);
+    });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    let searchWord = this.state.value;
+
+    let wikiUrl =
+      "https://commons.wikimedia.org/w/api.php?action=query&generator=images&prop=imageinfo&gimlimit=10&redirects=1&titles=" +
+      searchWord +
+      "&iiprop=canonicaltitle|url|size|dimensions&format=json";
+    let wordnikUrl =
+      "http://api.wordnik.com/v4/word.json/" +
+      searchWord +
+      "/relatedWords?api_key=9ad74996eed8057e662010fa8ef0770fd099c0190d9f3f71f";
+
+    this.makeWordnikCall(wordnikUrl);
+    this.makeWikiCall(wikiUrl);
+  }
+
+  handleChange(e) {
+  	console.log(e);
+    this.setState({ value: e.target.value });
+  }
+
   render() {
+  	    const {relatedImages,relatedWords, value} = this.state;
+
     return (
       <div className="container">
         <Header />
-        <Search />
+        <Search searchQuery={value} onChange={this.handleChange} search={this.handleClick}/>
+        <ImageResult searchResult={relatedImages}/>
+        <TextResult searchResult={relatedWords}/>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,77 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
-import wtf from "wtf_wikipedia";
 
 class Search extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      relatedWords: [],
-      relatedImages: [],
-      value: "sun"
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+  super(props);
   }
 
-  makeWordnikCall(url) {
-    axios.get(url).then(res => {
-      let relatedWords = res.data[res.data.length - 1].words;
-      this.setState({ relatedWords });
-    });
-  }
-
-  makeWikiCall(url) {
-    axios.get(url).then(res => {
-      let result = res.data.query.pages;
-      console.log(result);
-      let pageId = [];
-      let relatedImages = [];
-
-      // getting the ID of the image object
-      for (var keys in result) {
-        pageId.push(keys);
-      }
-      console.log(pageId);
-
-      // get the list of image names
-      for (var i = 0; i < pageId.length; i++) {
-        console.log(result[pageId[i]].imageinfo[0].url);
-        var imageUrl = result[pageId[i]].imageinfo[0].url;
-        relatedImages.push(imageUrl);
-      }
-      // for (let i = 0; i < result[pageId].images.length; i++) {
-      //   // imageUrl = imageUrl.replace(" ", "_");
-      //   // console.log(imageUrl);
-      //   // relatedImages.push(imageUrl);
-      // }
-
-      this.setState({ relatedImages });
-
-      console.log("rel img ", relatedImages);
-    });
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    let searchWord = this.state.value;
-
-    let wikiUrl =
-      "https://commons.wikimedia.org/w/api.php?action=query&generator=images&prop=imageinfo&gimlimit=10&redirects=1&titles=" +
-      searchWord +
-      "&iiprop=canonicaltitle|url|size|dimensions&format=json";
-    let wordnikUrl =
-      "http://api.wordnik.com/v4/word.json/" +
-      searchWord +
-      "/relatedWords?api_key=9ad74996eed8057e662010fa8ef0770fd099c0190d9f3f71f";
-
-    this.makeWordnikCall(wordnikUrl);
-    this.makeWikiCall(wikiUrl);
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
+  
 
   render() {
     return (
@@ -84,33 +19,15 @@ class Search extends Component {
                   className="input"
                   type="text"
                   placeholder="What word have you learned?"
-                  value={this.state.value}
-                  onChange={this.handleChange}
+                  value={this.props.searchQuery}
+                  onChange={this.props.onChange}
                 />
               </div>
 
               <div className="column is-one-quarter">
-                <button onClick={this.handleClick} className="button">
+                <button onClick={this.props.search} className="button">
                   Find!
                 </button>
-              </div>
-            </div>
-            <div className="columns">
-              <div className="column">
-                <ul>
-                  {this.state.relatedWords.map(relatedWord => (
-                    <li>{relatedWord}</li>
-                  ))}
-                </ul>
-              </div>
-              <div className="column">
-                <ul>
-                  {this.state.relatedImages.map(relatedImage => (
-                    <li>
-                      <img src={relatedImage} />
-                    </li>
-                  ))}
-                </ul>
               </div>
             </div>
           </div>
